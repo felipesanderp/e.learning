4cb024d0f3660c339bb9f74b2da0cc2a2ddf4529
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-04 14:23:08 -0300

    icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,36 @@
+import {
+  Laptop,
+  Moon,
+  SunMedium,
+  Command,
+  GraduationCap,
+  X,
+  type Icon as LucideIcon,
+  ArrowRight,
+  Plus,
+  Loader2,
+  BookOpen,
+  Facebook,
+  User,
+  LogOut,
+  Home
+} from "lucide-react"
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  sun: SunMedium,
+  moon: Moon,
+  laptop: Laptop,
+  logo: Command,
+  graduationCap: GraduationCap,
+  close: X,
+  arrowRight: ArrowRight,
+  add: Plus,
+  spinner: Loader2,
+  bookOpen: BookOpen,
+  facebook: Facebook,
+  user: User,
+  logOut: LogOut,
+  home: Home
+}
