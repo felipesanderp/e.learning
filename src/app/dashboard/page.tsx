30edb326ec4a763d09162f72521ea34611eb3365
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-05 15:07:40 -0300

    page metadata

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,10 @@ import { redirect } from "next/navigation"
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
 
+export const metadata = {
+  title: 'Dashboard | e.learning',
+}
+
 export default async function DashboardPage() {
   const user = await getCurrentUser()
 
