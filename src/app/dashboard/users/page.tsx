f83172d0cf55efeae11bc3cce237f98a501ed95e
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-09 16:32:29 -0300

    users initial page

---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+
+import { DashboardHeader } from "@/components/header";
+import { DashboardShell } from "@/components/shell";
+import { getCurrentUser } from "@/lib/session";
+import { authOptions } from "@/lib/auth";
+
+export default async function Users() {
+  const user = await getCurrentUser()
+
+  if (!user || user.role !== 'ADMIN' && user.role !== 'PROFESSOR') {
+    redirect(authOptions?.pages?.signIn || "/")
+  }
+  
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        heading='Users'
+        text='Create and manage users.'
+      />
+    </DashboardShell>
+  )
+}
