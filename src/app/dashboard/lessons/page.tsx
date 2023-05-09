dc807de12bb69344aa1e637900bb6404a4b4fd4e
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-09 16:33:30 -0300

    check if user is admin or professor and redirect if not

--- src/app/dashboard/lessons/page.tsx
+++ src/app/dashboard/lessons/page.tsx
@@ -2,15 +2,17 @@ import { redirect } from "next/navigation"
 
 import { db } from "@/lib/db"
 import { getCurrentUser } from "@/lib/session";
+import { authOptions } from "@/lib/auth";
 import { DashboardHeader } from "@/components/header";
 import { DashboardShell } from "@/components/shell";
 
 export default async function LessonsPage() {
   const user = await getCurrentUser()
 
-  if (!user) {
-    redirect("/")
+  if (!user || user.role !== 'ADMIN' && user.role !== 'PROFESSOR') {
+    redirect(authOptions?.pages?.signIn || "/")
   }
+  
   return (
     <DashboardShell>
       <DashboardHeader
