ea4dfab5a0d1bfbebf971645f64221d5adab8acb
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-10 12:21:39 -0300

    context menu to edit and delete course

--- src/components/courses-card.tsx
+++ src/components/courses-card.tsx
@@ -8,6 +8,16 @@ import {
   CardHeader, 
   CardTitle,
 } from "@/components/ui/card";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
 import { Button } from './ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import Link from 'next/link';
@@ -21,49 +31,60 @@ interface CoursesCardProps {
 
 export function CoursesCard({ course }: CoursesCardProps) {
   return (
-    <Card className="w-[300px]">
-      <CardHeader className="p-2">
-        <div className="relative rounded-md border w-full h-[150px]">
-          <Image 
-            src={course.imageURL}
-            alt={course.title}
-            fill
-            quality={50}
-            className="rounded-md"
-          />
-        </div>
+    <ContextMenu>
+      <Card className="w-[300px]">
+        <CardHeader className="p-2">
+        <ContextMenuTrigger>
+          <div className="relative rounded-md border w-full h-[150px]">
+            <Image 
+              src={course.imageURL}
+              alt={course.title}
+              fill
+              quality={50}
+              className="rounded-md"
+            />
+          </div>
+        </ContextMenuTrigger>
+          <CardTitle>
+            {course.title}
+          </CardTitle>
 
-        <CardTitle>
-          {course.title}
-        </CardTitle>
+          <CardDescription>
+            Lessons
+          </CardDescription>
+        </CardHeader>
 
-        <CardDescription>
-           Lessons
-        </CardDescription>
-      </CardHeader>
+        <ScrollArea className="h-[72px]">
+          <CardContent>
+            <p>{course.description}</p>
+          </CardContent>
+        </ScrollArea>
 
-      <ScrollArea className="h-[72px]">
-        <CardContent>
-          <p>{course.description}</p>
-        </CardContent>
-      </ScrollArea>
-
-      <CardFooter className="justify-between p-4">
-        <div className="flex items-center gap-2">
-          <Avatar>
-            <AvatarImage alt="" src='https://github.com/diego3g.png'/>
-            <AvatarFallback>
-              <span className="sr-only">Diego S.</span>
-            </AvatarFallback>
-          </Avatar>
-          Diego Shell
-        </div>
-        <Link href={`/dashboard/courses/${course.id}`}>
-          <Button>
-            Acessar
-          </Button>
-        </Link> 
-      </CardFooter>
-    </Card>
+        <CardFooter className="justify-between p-4">
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage alt="" src='https://github.com/diego3g.png'/>
+              <AvatarFallback>
+                <span className="sr-only">Diego S.</span>
+              </AvatarFallback>
+            </Avatar>
+            Diego Shell
+          </div>
+          <Link href={`/dashboard/courses/${course.id}`}>
+            <Button>
+              Acessar
+            </Button>
+          </Link> 
+        </CardFooter>
+      </Card>
+      
+      <ContextMenuContent>
+        <ContextMenuItem>Edit</ContextMenuItem>
+        <ContextMenuSeparator />
+        <ContextMenuItem>
+          Delete
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
