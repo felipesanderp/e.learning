4f8bf7e7c499e3bc869bb8094f1e10b90f92c91e
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-16 11:30:49 -0300

    vide component

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { VideoPlayer } from '@/hooks/use-video'
+import { ArrowRight, File, Lightbulb, Slack } from 'lucide-react'
 import Image from 'next/image'
 
 export function Video() {
   return (
     <div className="flex-1">
-      <div className="bg-slate-400 flex justify-center">
+      <div className="flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <VideoPlayer key="1" videoUrl="https://www.youtube.com/watch?v=3He7tLmtyrg" />
         </div>
@@ -18,7 +19,7 @@ export function Video() {
             <h1 className="text-2xl font-bold">
               ReactJS
             </h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">
+            <p className="mt-4 text-primary leading-relaxed">
               ReactJS Course for people who want to dominate the new tools of the front-end development.
             </p>
 
@@ -31,12 +32,56 @@ export function Video() {
                 className='rounded-full border-2 border-blue-500'
               />
 
-              <div className="leading-relaxed">
+              <div className="flex flex-col leading-relaxed">
                 <strong>Diego Fernandes</strong>
                 <span>founder founder founder</span>
               </div>
             </div>
           </div>
+
+          <div className="flex flex-col gap-4">
+            <a href="#" className="text-secondary p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <Slack />
+              Comunidade Discord
+            </a>
+
+            <a href="#" className="text-secondary p-4 text-sm bg-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-700 transition-colors">
+              <Lightbulb />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <File />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl text-secondary">Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <ArrowRight />
+            </div>
+          </a>
+
+          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <File />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl text-secondary">Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <ArrowRight />
+            </div>
+          </a>
         </div>
       </div>
     </div>
