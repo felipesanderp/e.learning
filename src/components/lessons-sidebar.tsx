407bd0ce7da03264fbfe30a36fcf1405e9ae3842
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-16 11:52:01 -0300

    change sidebar color

--- src/components/lessons-sidebar.tsx
+++ src/components/lessons-sidebar.tsx
@@ -2,7 +2,7 @@ import { LessonCard } from "./lesson-card";
 
 export function LessonsSidebar() {
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="w-[348px] bg-background p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de Aulas
       </span>
