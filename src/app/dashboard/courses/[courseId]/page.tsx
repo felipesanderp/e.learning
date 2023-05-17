9704c60e8dc30660933327671c3f630b929698a9
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-17 12:06:47 -0300

    check if there is a user log in

--- src/app/dashboard/courses/[courseId]/page.tsx
+++ src/app/dashboard/courses/[courseId]/page.tsx
@@ -1,11 +1,18 @@
-import { notFound } from "next/navigation"
-
-import { db } from "@/lib/db"
+import { notFound, redirect } from "next/navigation"
 import { Courses } from "@prisma/client"
+
 import { Video } from "@/components/video"
 import { LessonsSidebar } from "@/components/lessons-sidebar"
+import { getCurrentUser } from "@/lib/session"
+import { db } from "@/lib/db"
 
 async function getCourse(courseId: Courses["id"]) {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect('/')
+  }
+
   return await db.courses.findFirst({
     where: {
       id: courseId,
