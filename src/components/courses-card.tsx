91bedd37cf6aede70578d980ce58e5eafa2867ae
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-26 12:07:01 -0300

    delete course function

--- src/components/courses-card.tsx
+++ src/components/courses-card.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import Image from 'next/image'
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Courses, User } from '@prisma/client';
 
 import { 
@@ -17,11 +21,42 @@ import {
   ContextMenuSeparator,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
 import { Button } from './ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Icons } from './icons';
+import { toast } from '@/hooks/use-toast';
 
+async function deleteCourse(courseId: string) {
+  const response = await fetch(`/api/courses/${courseId}`, {
+    method: 'DELETE',
+  })
+
+  if(!response.ok) {
+    toast({
+      title: "Something went wrong.",
+      description: "The course was not deleted. Please try again.",
+      variant: "destructive",
+    })
+  }
+
+  toast({
+    title: "Course deleted.",
+    variant: "success"
+  })
+
+  return true
+}
 
 interface CoursesCardProps {
   course: Pick<Courses, "id" | "title" | "imageURL" | "description" | "slug">
@@ -29,6 +64,10 @@ interface CoursesCardProps {
 }
 
 export function CoursesCard({ course, user }: CoursesCardProps) {
+  const router = useRouter()
+  const [showDeleteAlert, setShowDeleteAlert] = useState<boolean>(false)
+  const [isDeleteLoading, setIsDeleteLoading] = useState<boolean>(false)
+
   return (
     <ContextMenu>
       <Card className="w-[300px]">
@@ -92,12 +131,54 @@ export function CoursesCard({ course, user }: CoursesCardProps) {
             Edit
           </ContextMenuItem>
           <ContextMenuSeparator />
-          <ContextMenuItem className="text-red-500 focus:bg-red-200 focus:text-bg-500">
+          <ContextMenuItem 
+            onSelect={() => setShowDeleteAlert(true)}
+            className="text-red-500 focus:bg-red-200 focus:text-bg-500"
+          >
             <Icons.trash className="mr-2 h-3.5 w-3.5 text-red-500" />
             Delete
           </ContextMenuItem>
         </ContextMenuContent>
       ) : ''}
+
+      {/* Alert dialog for delete course */}
+      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Você tem certeza que deseja deletar este curso?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Essa ação não poderá ser desfeita.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async (event) => {
+                event?.preventDefault()
+                setIsDeleteLoading(true)
+                
+                const deleted = await deleteCourse(course.id)
+
+                if(deleted) {
+                  setIsDeleteLoading(false)
+                  setShowDeleteAlert(false)
+                  router.refresh()
+                }
+              }}
+              className="bg-red-600 focus:ring-red-600"
+            >
+               {isDeleteLoading ? (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <Icons.trash className="mr-2 h-4 w-4" />
+              )}
+              <span>Delete</span>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </ContextMenu>
   )
 }
