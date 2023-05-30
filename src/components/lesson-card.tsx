7e8eeed8151351bd5411b140e8024302ab9dcdb5
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-30 12:01:42 -0300

    change width and color when is active

--- src/components/lesson-card.tsx
+++ src/components/lesson-card.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Circle } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { CheckCircle } from "lucide-react";
+import { useRouter, usePathname } from "next/navigation";
 
 interface LessonCard {
   name: string
@@ -11,6 +12,7 @@ interface LessonCard {
 
 export function LessonCard({ name, slug, courseId }: LessonCard) {
   const router = useRouter()
+  const pathname = usePathname()
   
   const handleClick = (courseId: string, slug: string) => {
     router.push(`/dashboard/courses/${courseId}/${slug}`)
@@ -19,10 +21,13 @@ export function LessonCard({ name, slug, courseId }: LessonCard) {
   return (
     <div>
       <button className="group" onClick={() => handleClick(courseId, slug)}>
-        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <div className={cn(
+          "w-[18rem] rounded border border-gray-500 p-4 mt-2 group-hover:border-purple-500",
+          pathname === (`/dashboard/courses/${courseId}/${slug}`) ? 'bg-purple-100 border-purple-500 dark:bg-purple-900' : 'border-gray-500'
+        )}>
           <header className="flex items-center justify-between">
             <span className="text-sm font-medium flex items-center gap-2">
-              <Circle />
+              <CheckCircle />
               Conteúdo liberado
             </span>
           </header>
--- src/components/lessons-sidebar.tsx
+++ src/components/lessons-sidebar.tsx
@@ -18,14 +18,19 @@ interface LessonsSidebarProps {
 
 export function LessonsSidebar({ course }: LessonsSidebarProps) {
   return (
-    <aside className="w-[348px] bg-background p-6 border-l border-gray-600">
-      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+    <aside className="w-[348px] min-h-screen bg-background p-6 border-l border-zinc-400">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-zinc-400 block">
         Cronograma de Aulas
       </span>
 
       <div className="flex flex-col gap-8">
         {course.lessons.map((lesson) => (
-          <LessonCard key={lesson.id} name={lesson.name} slug={lesson.slug} courseId={course.id}/>
+          <LessonCard 
+            key={lesson.id} 
+            name={lesson.name} 
+            slug={lesson.slug} 
+            courseId={course.id}
+          />
         ))}
       </div>
     </aside>
