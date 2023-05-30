72ad7c328db6449e539e191f26f861b7da36d131
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-30 15:38:07 -0300

    function to delete lesson

--- src/app/dashboard/lessons/components/data-table-row-actions.tsx
+++ src/app/dashboard/lessons/components/data-table-row-actions.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import * as React from 'react'
+import { useRouter } from 'next/navigation'
 import { Row } from "@tanstack/react-table"
-import { MoreHorizontal, Pen, Trash } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -9,9 +10,41 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import { toast } from '@/hooks/use-toast'
+import { Icons } from '@/components/icons'
+
+async function deleteLesson(lessonId: string) {
+  const response = await fetch(`/api/lesson/${lessonId}`, {
+    method: 'DELETE',
+  })
+
+  if(!response.ok) {
+    toast({
+      title: "Something went wrong.",
+      description: "The lesson was not deleted. Please try again.",
+      variant: "destructive",
+    })
+  }
+
+  toast({
+    title: "Lesson deleted.",
+    variant: "success"
+  })
+
+  return true
+}
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
@@ -20,29 +53,76 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
+  const router = useRouter()
+  const [showDeleteAlert, setShowDeleteAlert] = React.useState<boolean>(false)
+  const [isDeleteLoading, setIsDeleteLoading] = React.useState<boolean>(false)
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-        >
-          <MoreHorizontal className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>
-          <Pen className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-          Edit
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Trash className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          >
+            <Icons.moreDots className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[160px]">
+          <DropdownMenuItem>
+            <Icons.pen className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onSelect={() => setShowDeleteAlert(true)}
+          >
+            <Icons.trash className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+    {/* Alert dialog for delete user */}
+      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Você tem certeza que deseja deletar este usuário?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              Essa ação não poderá ser desfeita.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async (event) => {
+                event?.preventDefault()
+                setIsDeleteLoading(true)
+            
+                // @ts-expect-error
+                const deleted = await deleteLesson(row.original.id)
+
+                if(deleted) {
+                  setIsDeleteLoading(false)
+                  setShowDeleteAlert(false)
+                  router.refresh()
+                }
+              }}
+              className="bg-red-600 focus:ring-red-600"
+            >
+              {isDeleteLoading ? (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <Icons.trash className="mr-2 h-4 w-4" />
+              )}
+              <span>Delete</span>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+  </>
   )
 }
