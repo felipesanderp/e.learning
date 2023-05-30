4196d418ae4d145ccf7d838a28b8a60d556e718b
Felipe Sander <felipe.panisson@hotmail.com>
2023-05-30 09:38:38 -0300

    render preview button

--- src/app/dashboard/courses/edit/[courseId]/page.tsx
+++ src/app/dashboard/courses/edit/[courseId]/page.tsx
@@ -1,6 +1,8 @@
 import { EditCourseForm } from "@/components/edit-course-form"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 interface EditCoursePageProps {
   params: { courseId: string }
@@ -12,7 +14,11 @@ export default async function EditCoursePage({ params }:EditCoursePageProps) {
       <DashboardHeader 
         heading="Manage Course"
         text="Edit, delete and add new items to this course only."
-      />
+      >
+        <Link href={`/dashboard/courses/${params.courseId}`}>
+          <Button variant="link">Preview</Button>
+        </Link>
+      </DashboardHeader>
       <EditCourseForm 
         courseId={params.courseId}
       />
