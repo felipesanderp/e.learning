af1bf0377b7bfe58395072aaf4f2f4809656ef87
Felipe Sander <felipe.panisson@hotmail.com>
2023-06-02 16:48:14 -0300

    add spinner when data is been retrieving

--- src/components/admin/latest-created-users.tsx
+++ src/components/admin/latest-created-users.tsx
@@ -12,14 +12,18 @@ type users = Pick<User, "id" | "name" | "email" | "image" | "role">
 
 export function LatestCreatedUsers() {
   const [latestUsers, setLatestUsers] = useState<users[]>()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   useEffect(() => {
     async function getLatestUsers() {
+      setIsLoading(true)
+
       const response = await fetch('/api/users/latest-created', {
         method: 'GET',
         next: { revalidate: 3600 }
       })
       setLatestUsers(await response.json())
+      setIsLoading(false)
     }
 
     getLatestUsers()
@@ -27,28 +31,32 @@ export function LatestCreatedUsers() {
 
   return (
     <div className="space-y-8">
-      {latestUsers?.map((user) => (
-        <div className="flex items-center" key={user.id}>
-          <Avatar className="h-9 w-9">
-            {user.image && (
-              <AvatarImage src={user.image} alt="Avatar" />
-            )}
-            <AvatarFallback>
-              <span className="sr-only">{user.name}</span>
-              <Icons.user className="h-4 w-4" />
-            </AvatarFallback>
-          </Avatar>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">{user.name}</p>
-            <p className="text-sm text-muted-foreground">
-              {user.email}
-            </p>
+      {isLoading ? (
+        <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
+      ) : (
+        latestUsers?.map((user) => (
+          <div className="flex items-center" key={user.id}>
+            <Avatar className="h-9 w-9">
+              {user.image && (
+                <AvatarImage src={user.image} alt="Avatar" />
+              )}
+              <AvatarFallback>
+                <span className="sr-only">{user.name}</span>
+                <Icons.user className="h-4 w-4" />
+              </AvatarFallback>
+            </Avatar>
+            <div className="ml-4 space-y-1">
+              <p className="text-sm font-medium leading-none">{user.name}</p>
+              <p className="text-sm text-muted-foreground">
+                {user.email}
+              </p>
+            </div>
+            <Badge className="ml-auto font-medium">
+              {user.role}
+            </Badge>
           </div>
-          <Badge className="ml-auto font-medium">
-            {user.role}
-          </Badge>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   )
 }
