55d9759fbaec383190ba3ddd92ef5d3b57bda31a
Felipe Sander <felipe.panisson@hotmail.com>
2023-06-15 14:21:13 -0300

    dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -63,9 +63,11 @@ export default async function DashboardPage() {
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
       </div>
-      <Tabs defaultValue="overview" className="space-y-4">
+      <Tabs defaultValue={user.role === 'ADMIN' || user.role === 'PROFESSOR' ? 'overview' : 'my-courses'} className="space-y-4">
         <TabsList>
-          <TabsTrigger value="overview">Overview</TabsTrigger>
+          {user.role === 'ADMIN' || user.role === 'PROFESSOR' && (
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+          )}
           {user.role === 'STUDENT' && (
             <TabsTrigger value="my-courses">My Courses</TabsTrigger>
           )}
