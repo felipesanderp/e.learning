4bd38b16526a264c9c81f3de665a984a8e9020e0
Felipe Sander <felipe.panisson@hotmail.com>
2023-06-15 16:13:58 -0300

    bio update api

--- src/app/api/users/me/[meId]/route.ts
+++ src/app/api/users/me/[meId]/route.ts
@@ -69,6 +69,7 @@ export async function PATCH(
         email: payload.email,
         name: payload.name,
         image: payload.image,
+        bio: payload.bio,
         imageKey: payload.imageKey,
       }
     })
